b6c6ac19939a648b8e5b55f576230a81c326adec
Ahmad Safar <ahmadsafar88@gmail.com>
2021-05-24 02:31:20 +0800

    feat: add eslint, prettier, husky (auto format pre-commit)

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react-app',
+    'jest',
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    'no-param-reassign': 'off',
+    'import/prefer-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/button-has-type': 'off',
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
-} from "react-router-dom";
-import { Home } from "./pages/home";
-import { About } from "./pages/about";
+  Link,
+} from 'react-router-dom';
+import { Home } from './pages/home';
+import { About } from './pages/about';
 
 export default function BasicExample() {
   return (
