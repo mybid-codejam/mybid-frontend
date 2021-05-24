0943c06c768a659fe13b71686e140f738d60631c
Ahmad Safar <ahmadsafar88@gmail.com>
2021-05-24 11:14:53 +0800

    style: cleanup

--- src/components/layout/AppLayout.js
+++ src/components/layout/AppLayout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Transition } from '@headlessui/react';
 import { NavLink } from 'react-router-dom';
 import useOnClickOutside from 'use-onclickoutside';
